08365688ecddb7369ae4f1eec228e99d18fb6876
Isaac Violette <isaac.violette@maine.edu>
2023-04-24 15:58:16 -0400

    New database with date/time entry and kelvin entry. Script now works with database

--- data_query.py
+++ data_query.py
@@ -4,7 +4,7 @@ import serial
 import datetime
 
 #Get the time and date of data point in UTC
-time = datetime.datetime.utcnow()
+dt = datetime.datetime.utcnow()
 
 #Send byte sequence over serial, read temperature output as a float
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 5)
@@ -19,7 +19,7 @@ c = s.cursor()
 #Insert data into sql database, if it doesn't work, print error message
 #temp in kelvin
 try:
-    c.execute("insert into data values (?,?,?);",(temp_f,date,time))
+    c.execute("insert into data values (?,?);",(dt, temp_kelvin))
     s.commit()
 except:
     print('Error storing data in database');
