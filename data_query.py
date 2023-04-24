eeb5605125fc4cc86fd08ef5e796c915d73365aa
Isaac Violette <isaac.violette@maine.edu>
2023-04-24 15:47:03 -0400

    Kept data in kelvin, made utc time, need to change database input to date/time and temp

--- data_query.py
+++ data_query.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sqlite3
 import serial
-import time
+import datetime
 
-#Get the time and date of data point
-date = time.strftime("%Y-%m-%d")
-time = time.strftime("%H:%M:%S")
+#Get the time and date of data point in UTC
+time = datetime.datetime.utcnow()
 
 #Send byte sequence over serial, read temperature output as a float
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 5)
@@ -13,14 +12,12 @@ ser.write(b'GET DATA')
 temp_kelvin = float(ser.readline().decode().strip())
 ser.close()
 
-#Convert kelvin to fahrenheit
-temp_f = 1.8*temp_kelvin - 459.4
-
 #Open database connection
 s = sqlite3.connect('data.db')
 c = s.cursor()
 
 #Insert data into sql database, if it doesn't work, print error message
+#temp in kelvin
 try:
     c.execute("insert into data values (?,?,?);",(temp_f,date,time))
     s.commit()
