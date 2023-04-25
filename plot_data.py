20fb4cb71149d2262535150aa31dcd46d56d3264
Isaac Violette <isaac.violette@maine.edu>
2023-04-25 15:00:51 -0400

    Plot is now the right size, need to smooth plot out

--- plot_data.py
+++ plot_data.py
@@ -10,7 +10,7 @@ import numpy as np
 time_now = datetime.now()
 dayAgo = datetime.now() - timedelta(hours=24)
 
-s = sqlite3.connect('data.db')
+s = sqlite3.connect('/home/iviolette/datalogger/data.db')
 c = s.cursor()
 
 try:
@@ -19,8 +19,9 @@ try:
 except:
     print("Could not retrieve data from database")
 
-time_data = []
+datetime_data = []
 temp_data = []
+time_data = []
 for row in c.fetchall():
     temp_f = (row[1] - 273.15) * 9/5 + 32
     UTC_time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
@@ -28,22 +29,28 @@ for row in c.fetchall():
     UTC_time = UTC_tz.localize(UTC_time)
     EST_tz = pytz.timezone('US/Eastern')
     EST_time = UTC_time.astimezone(EST_tz)
-    EST_time_string = EST_time.strftime('%Y-%m-%d %H:%M')
-    time_data.append(EST_time_string)
+    EST_datetime_string = EST_time.strftime('%Y-%m-%d %H:%M')
+    EST_time_string = EST_time.strftime('%H:%M')
+    datetime_data.append(EST_datetime_string)
     temp_data.append(temp_f)
+    time_data.append(EST_time_string)
 
 s.close()
 
-time_np = np.array(time_data)
+datetime_np = np.array(datetime_data)
 temp_np = np.array(temp_data)
+time_np = np.array(time_data)
 
+plt.figure(figsize=(14,6))
 plt.plot(time_np,temp_np)
 
-xticks = np.linspace(0, 1440, 5)
+xticks = np.linspace(0, 1440, 13)
 plt.xticks(xticks,rotation=15,fontsize=10)
 
-plt.xlabel('Date and Time')
+plt.xlabel('Time')
 plt.ylabel('Temperature (F)')
-plt.title(f'Temperature from {time_np[0]} to {time_np[-1]}')
-plt.savefig("/var/www/html/temp_plot.png", dpi=800)
+plt.title(f'Temperature from {datetime_np[0]} to {datetime_np[-1]}')
+plt.grid()
+plt.savefig("/var/www/html/temp_plot.png", dpi=250)
+
 
