e2e1f31ec9f1073e7aec8694ee71e8f8c248fbea
Isaac Violette <isaac.violette@maine.edu>
2023-04-24 21:14:28 -0400

    Finally after many tests, data is converted properly and ready for plotting

--- plot_data.py
+++ plot_data.py
@@ -2,7 +2,8 @@
 
 import sqlite3
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+import pytz
 
 time_now = datetime.now()
 dayAgo = datetime.now() - timedelta(hours=24)
@@ -11,16 +12,22 @@ s = sqlite3.connect('data.db')
 c = s.cursor()
 
 try:
-    c.execute("select * from data where datetime >= '{dayAgo}';")
+    c.execute("select * from data where datetime >= ?", (dayAgo,))
     s.commit()
 except:
     print("Could not retrieve data from database")
 
-# data = []
-# for row in c.fetchall():
-#    print(row[0])
-#    print(row[1])
+data = []
+for row in c.fetchall():
+    temp_f = (row[1] - 273.15) * 9/5 + 32
+    UTC_time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f')
+    UTC_tz = pytz.timezone('UTC')
+    UTC_time = UTC_tz.localize(UTC_time)
+    EST_tz = pytz.timezone('US/Eastern')
+    EST_time = UTC_time.astimezone(EST_tz)
+    EST_time_string = EST_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+    data.append((EST_time_string, temp_f))
 
-print(c.fetchall())
+print(data)
 
 s.close()
