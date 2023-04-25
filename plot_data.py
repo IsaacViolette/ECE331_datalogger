14f65d202d5d5c6d1e0e47c8cf9f95759b49d7e0
Isaac Violette <isaac.violette@maine.edu>
2023-04-24 21:55:05 -0400

    Made plot much nicer, need to get enough data to fill entire plot

--- plot_data.py
+++ plot_data.py
@@ -27,16 +27,21 @@ for row in c.fetchall():
     UTC_time = UTC_tz.localize(UTC_time)
     EST_tz = pytz.timezone('US/Eastern')
     EST_time = UTC_time.astimezone(EST_tz)
-    EST_time_string = EST_time.strftime('%Y-%m-%d %H:%M:%S.%f')
+    EST_time_string = EST_time.strftime('%Y-%m-%d %H:%M')
     time_data.append(EST_time_string)
     temp_data.append(temp_f)
 
 s.close()
 
+
 time_np = np.array(time_data)
 temp_np = np.array(temp_data)
 
 plt.plot(time_np,temp_np)
+
+xticks = np.linspace(0, 1440, 5)
+plt.xticks(xticks,rotation=25,fontsize=10)
+
 plt.xlabel('Date and Time')
 plt.ylabel('Temperature (F)')
 plt.title('Temperature Over Time')
